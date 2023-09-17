d7a51efcad805ce7c29325d8bcb5debbe296d7c9
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-17 15:55:22 +0300

    adding task 6

--- models/base_model.py
+++ models/base_model.py
@@ -12,12 +12,14 @@ Base = declarative_base()
 class BaseModel:
     """A base class for all hbnb models"""
     id = Column(String(60), nullable=False, primary_key=True)
+    created_at = Column(DateTime, nullable=False, default=datetime.utcnow())
+    updated_at = Column(DateTime, nullable=False, default=datetime.utcnow())
     
 
     def __init__(self, *args, **kwargs):
         """Instatntiates a new model"""
         if not kwargs:
-            from models import storage
+            
             self.id = str(uuid.uuid4())
             self.created_at = datetime.now()
             self.updated_at = datetime.now()
@@ -58,6 +60,8 @@ class BaseModel:
         dictionary.update(self.__dict__)
         dictionary.update({'__class__':
                         (str(type(self)).split('.')[-1]).split('\'')[0]})
+        if "_sa_instance_state" in dictionary:
+            del dictionary["_sa_instance_state"]
         if isinstance(self.created_at, datetime):
             dictionary['created_at'] = self.created_at.isoformat()
         else:
@@ -67,3 +71,8 @@ class BaseModel:
         else:
             dictionary['updated_at'] = self.updated_at
         return dictionary
+
+    def delete(self):
+        """Delete the current instance"""
+        from models import storage
+        storage.delete(self)
---
+++ models/engine/db_storage.py
@@ -0,0 +1,77 @@
+#!/usr/bin/python3
+"""
+DBStorage Engine Model to store the data
+"""
+from sqlalchemy import create_engine
+import os
+from models.base_model import Base, BaseModel
+from sqlalchemy.orm import sessionmaker, scoped_session
+from models.amenity import Amenity
+from models.city import City
+from models.place import Place
+from models.review import Review
+from models.state import State
+from models.user import User
+
+
+user = os.environ.get('HBNB_MYSQL_USER')
+pwd = os.environ.get('HBNB_MYSQL_PWD')
+host = os.environ.get('HBNB_MYSQL_HOST')
+db = os.environ.get('HBNB_MYSQL_DB')
+
+class DBStorage:
+    """
+    DB Storage to store the data
+    """
+    __engine = None
+    __session = None
+    classes = [City, Review, User, Amenity, State, Place]
+    
+    def __init__(self):
+        """
+        Initialize the DB Storage
+        """
+        self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
+                                        format(user, pwd, host, db),
+                                        pool_pre_ping=True)
+        if os.environ.get('HBNB_ENV') == 'test':
+            Base.metadata.drop_all(self.__engine)
+            
+    def all(self, cls=None):
+        """
+        query on the current database session
+        """
+        new_dict = {}
+        if cls in None:
+            for clss in self.classes:
+                objs = self.__session.query(clss).all()
+                for obj in objs:
+                    key = obj.__class__.__name__ + '.' + obj.id
+                    new_dict[key] = obj
+        elif cls in self.classes:
+            objs = self.__session.query(cls).all()
+            for obj in objs:
+                key = obj.__class__.__name__ + '.' + obj.id
+                new_dict[key] = obj
+        return new_dict
+    
+    def new(self, obj):
+        """Adds object to current db session"""
+        self.__session.add(obj)
+        
+    def save(self):
+        """Commits all changes"""
+        self.__session.commit()
+        
+    def delete(self, obj=None):
+        """Deletes from current database session if obj is not None"""
+        if obj is not None:
+            self.__session.delete(obj)
+            self.__session.commit()
+    
+    def reload(self):
+        """Creates all tables in database"""
+        Base.metadata.create_all(self.__engine)
+        session = sessionmaker(bind=self.__engine, expire_on_commit=False)
+        Session = scoped_session(session)
+        self.__session = Session()

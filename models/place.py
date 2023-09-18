71f63ac22ec8f37ea3b4dc2812c087b6e2566609
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-18 08:27:30 +0300

    fixing amenities in place class

--- models/place.py
+++ models/place.py
@@ -30,44 +30,41 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-    reviews = relationship(
+    amenity_ids = []
+    
+    if db_type == 'db':
+        reviews = relationship(
             'Review', backref='place', cascade='all, delete')
-    amenities = relationship(
-            'Amenity', secondary=place_amenity,
-            viewonly=False)
+        amenities = relationship(
+                'Amenity', secondary=place_amenity,
+                viewonly=False)
+    else:
+        @property
+        def reviews(self):
+            """
+            getter for reviews for file storage
+            """
+            from models import storage
+            from models.review import Review
+            reviews_list = []
+            _reviews = storage.all(Review).values()
+            for review in _reviews:
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list
 
-    @property
-    def reviews(self):
-        """
-        getter for reviews for file storage
-        """
-        from models import storage
-        from models.review import Review
-        reviews_list = []
-        _reviews = storage.all(Review).values()
-        for review in _reviews:
-            if review.place_id == self.id:
-                reviews_list.append(review)
-        return reviews_list
+        @property
+        def amenities(self):
+            """
+            getter for the aminites
+            """
+            return self.amenity_ids
 
-    @property
-    def amenities(self):
-        """
-        getter for the aminites
-        """
-        from models import storage
-        from models.amenity import Amenity
-        amenities_list = []
-        _amenities = storage.all(Amenity).values()
-        for amenity in _amenity:
-            if amenity.amenity_ids == self.id:
-                amenities_list.append(amenity)
-        return amenities_list
-
-    @amenities.setter
-    def amenities(self, obj):
-        """
-        setter for the amenintes
-        """
-        if isinstance(obj, Amenity):
-            self.amenity_ids.append(obj.id)
+        @amenities.setter
+        def amenities(self, obj):
+            """
+            setter for the amenintes
+            """
+            from models.amenity import Amenity
+            if isinstance(obj, Amenity):
+                self.amenity_ids.append(obj.id)

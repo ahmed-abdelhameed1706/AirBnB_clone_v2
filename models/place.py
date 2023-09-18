198a84fda1f33be672ce8d5f849cb463df31d389
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-09-17 21:13:28 -0700

    adding more classes

--- models/place.py
+++ models/place.py
@@ -2,11 +2,16 @@
 """ Place Module for HBNB project """
 from models.base_model import BaseModel, Base
 from sqlalchemy import Column, Integer, Float, String, ForeignKey
+import os
+from sqlalchemy.orm import relationship
+
+db_type = os.environ.get('HBNB_TYPE_STORAGE')
+
 
 class Place(BaseModel, Base):
     """ A place to stay """
     __tablename__ = 'places'
-    
+
     city_id = Column(String(60), ForeignKey('cities.id'), nullable=False)
     user_id = Column(String(60), ForeignKey('users.id'), nullable=False)
     name = Column(String(128), nullable=False)
@@ -17,4 +22,21 @@ class Place(BaseModel, Base):
     price_by_night = Column(Integer, default=0, nullable=False)
     latitude = Column(Float, nullable=False)
     longitude = Column(Float, nullable=False)
-    amenity_ids = []
+
+    if db_type == 'db':
+        reviews = relationship(
+                'Review', backref='place', cascade='all, delete')
+    else:
+        @property
+        def reviews(self):
+            """
+            getter for reviews for file storage
+            """
+            from models import storage
+            from models.review import Review
+            reviews_list = []
+            _reviews = storage.all(Review).values()
+            for review in _reviews:
+                if review.place_id == self.id:
+                    reviews_list.append(review)
+            return reviews_list

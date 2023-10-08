f64f3cac3ac710841eb3d771165c18e84b061404
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-08 01:17:03 -0700

    0-setup_web_static.sh

--- 0-setup_web_static.sh
+++ 0-setup_web_static.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
 # script to set up the web servers
 
-apt update -y
-apt install nginx -y
+sudo apt update -y
+sudo apt install nginx -y
 
 mkdir -p /data/web_static/releases/test/
 mkdir -p /data/web_static/shared/
@@ -13,6 +13,6 @@ ln -sf /data/web_static/releases/test/ /data/web_static/current
 
 chown -R ubuntu:ubuntu /data/
 
-sudo sed -i '$i\    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
+sudo sed -i '78i\    location /hbnb_static/ {\n        alias /data/web_static/current/;\n    }' /etc/nginx/sites-available/default
 
-service nginx restart
+sudo service nginx restart

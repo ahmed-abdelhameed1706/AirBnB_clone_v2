a9521a482e67d8f89ed4a947eb0b1ae4f00bb78e
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 07:11:27 -0700

    1-hbnb_route.py

--- web_flask/1-hbnb_route.py
+++ web_flask/1-hbnb_route.py
@@ -1,14 +1,28 @@
+#!/usr/bin/python3
+"""
+ a script that starts a Flask web application
+"""
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    Function to return something
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """
+    function to reutnr something
+    """
     return "HBNB"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)

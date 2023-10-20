5fe4782bd6b21c5c08dd5d8141bc7c2861f4b707
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 07:37:32 -0700

    adding files

--- web_flask/3-python_route.py
+++ web_flask/3-python_route.py
@@ -1,24 +1,38 @@
+#!/usr/bin/python3
+"""
+ a script that starts a Flask web application
+"""
 from flask import Flask
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """function to return text"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """function to return text"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """function to return texT"""
     return f"C {escape(text).replace('_', ' ')}"
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
+    """function to return text"""
     return f"Python {escape(text).replace('_', ' ')}"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)
--- web_flask/5-number_template.py
+++ web_flask/5-number_template.py
@@ -1,32 +1,50 @@
+#!/usr/bin/python3
+"""
+ a script that starts a Flask web application
+"""
 from flask import Flask, render_template
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """function to return text"""
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """function to return text"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """function to return texT"""
     return f"C {escape(text).replace('_', ' ')}"
+
 
 @app.route('/python', strict_slashes=False)
 @app.route('/python/<text>', strict_slashes=False)
 def python(text='is cool'):
+    """function to return text"""
     return f"Python {escape(text).replace('_', ' ')}"
+
 
 @app.route('/number/<int:n>', strict_slashes=False)
 def number(n):
+    """function to return a string"""
     return f"{n} is a number"
+
 
 @app.route('/number_template/<int:n>', strict_slashes=False)
 def number_template(n):
+    """function to render a template"""
     return render_template("5-number.html", n=n)
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)

31f7a5c8911f4ce8ae70ee515bb8e1103d41bf2d
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 09:24:03 -0700

    states files

--- web_flask/7-states_list.py
+++ web_flask/7-states_list.py
@@ -1,3 +1,7 @@
+#!/usr/bin/python3
+"""
+script that starts a Flask web application
+"""
 from flask import Flask, render_template
 from models import storage
 from models.state import State
@@ -5,17 +9,21 @@ from models.state import State
 
 app = Flask(__name__)
 
+
 @app.teardown_appcontext
-def remove_session():    
+def remove_session(e):
+    """closes session"""
     storage.close()
+
 
 @app.route('/states_list', strict_slashes=False)
 def show_states():
     """
     renders a page with all states
     """
-    states = storage.all("State").values()
+    states = list(storage.all(State).values())
     return render_template('7-states_list.html', states=states)
 
+
 if __name__ == '__main__':
-        app.run(host='0.0.0.0')
+    app.run(host='0.0.0.0', port=5000)

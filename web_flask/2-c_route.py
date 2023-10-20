8f0dde4f78b8204d59da27c393841d64ef2c7112
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 07:12:49 -0700

    2-c_route.py

--- web_flask/2-c_route.py
+++ web_flask/2-c_route.py
@@ -1,19 +1,33 @@
+#!/usr/bin/python3
+"""
+ a script that starts a Flask web application
+"""
 from flask import Flask
 from markupsafe import escape
 
+
 app = Flask(__name__)
+
 
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    function to return text
+    """
     return "Hello HBNB!"
+
 
 @app.route('/hbnb', strict_slashes=False)
 def hbnb():
+    """ function to return text"""
     return "HBNB"
+
 
 @app.route('/c/<text>', strict_slashes=False)
 def c(text):
+    """function to return text"""
     return f"C {escape(text).replace('_', ' ')}"
 
+
 if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+    app.run(host="0.0.0.0", port=5000)

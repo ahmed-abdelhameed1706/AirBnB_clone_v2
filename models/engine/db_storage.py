394f83c53c37238ab0c984bac47f38c09326769c
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 07:41:02 -0700

    adding files

--- models/engine/db_storage.py
+++ models/engine/db_storage.py
@@ -19,23 +19,24 @@ pwd = os.environ.get('HBNB_MYSQL_PWD')
 host = os.environ.get('HBNB_MYSQL_HOST')
 db = os.environ.get('HBNB_MYSQL_DB')
 
+
 class DBStorage:
     """
     DB Storage to store the data
     """
     __engine = None
     __session = None
-    
+
     def __init__(self):
         """
         Initialize the DB Storage
         """
         self.__engine = create_engine('mysql+mysqldb://{}:{}@{}/{}'.
-                                        format(user, pwd, host, db),
-                                        pool_pre_ping=True)
+                        format(user, pwd, host, db),
+                        pool_pre_ping=True)
         if os.environ.get('HBNB_ENV') == 'test':
             Base.metadata.drop_all(self.__engine)
-            
+
     def all(self, cls=None):
         """
         query on the current database session
@@ -44,36 +45,39 @@ class DBStorage:
         classes = [City, Review, User, Amenity, State, Place]
         if cls is None:
             for clss in classes:
-                
+
                 objs = self.__session.query(clss).all()
                 for obj in objs:
                     key = obj.__class__.__name__ + '.' + obj.id
                     new_dict[key] = obj
         elif cls in classes:
-            
+
             objs = self.__session.query(cls).all()
             for obj in objs:
                 key = obj.__class__.__name__ + '.' + obj.id
                 new_dict[key] = obj
         return new_dict
-    
+
     def new(self, obj):
         """Adds object to current db session"""
         self.__session.add(obj)
-        
+
     def save(self):
         """Commits all changes"""
         self.__session.commit()
-        
+
     def delete(self, obj=None):
         """Deletes from current database session if obj is not None"""
         if obj is not None:
             self.__session.delete(obj)
             self.__session.commit()
-    
+
     def reload(self):
         """Creates all tables in database"""
         Base.metadata.create_all(self.__engine)
         session = sessionmaker(bind=self.__engine, expire_on_commit=False)
         Session = scoped_session(session)
         self.__session = Session()
+
+    def close(self):
+        self.__session.close()
--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -64,3 +64,9 @@ class FileStorage:
             if key in FileStorage.__objects:
                 del FileStorage.__objects[key]
                 self.save()
+
+    def close(self):
+        """
+        call reload() method for deserializing the JSON file to objects
+        """
+        self.reload()

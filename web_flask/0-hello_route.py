1a867c032001b6f92e84aab72dd946e6215aea1b
ahmed-abdelhameed1706 <ahmed.abdelhameed1706@gmail.com>
2023-10-20 07:06:04 -0700

    0-hello_route.py

--- web_flask/0-hello_route.py
+++ web_flask/0-hello_route.py
@@ -1,10 +1,22 @@
+#!/usr/bin/python3
+"""
+a script that starts a Flask web application
+"""
+
+
 from flask import Flask
+
 
 app = Flask(__name__)
 
+
 @app.route('/', strict_slashes=False)
 def hello():
+    """
+    function to return Hello HBNB
+    """
     return "Hello HBNB!"
+
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
